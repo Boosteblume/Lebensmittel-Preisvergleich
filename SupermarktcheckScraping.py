5420d7ca9827d07a7cd8f929b77ff1f2cb746f48
„Ghost911-s“ <maxblum94@yahoo.de>
2020-12-18 11:29:24 +0100

    added Selenium & Website

--- SupermarktcheckScraping.py
+++ SupermarktcheckScraping.py
@@ -1,1 +1,9 @@
 #first idea
+# Website https://www.supermarktcheck.de/lebensmittel/
+
+
+from selenium import webdriver
+
+driver = webdriver.Safari()
+
+url=r"https://www.supermarktcheck.de/lebensmittel/"
