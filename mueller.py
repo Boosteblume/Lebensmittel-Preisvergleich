cb92f091e4c93ddb846b62cf6db7619e39c18bf5
Ghost911-s <maxblum94@yahoo.de>
2021-03-18 22:00:01 +0100

    works

--- mueller.py
+++ mueller.py
@@ -1,21 +1,12 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-import pandas as pd
-import numpy as np
-import matplotlib as mlt
-import matplotlib.pyplot as plt
 
-PATH = "/Users/sophia/Documents/Lebensmittel-Preisvergleich/chromedriver"
-
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome()
 
 #launch the Browser and enter the webiste
 #accept the cookies
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome()
 driver.get(r"https://www.mueller.de/drogerie/lebensmittel/nahrung/")
 time.sleep(2) 
 acceptframe = driver.find_element_by_id("uc-btn-accept-banner").click()
@@ -45,13 +36,12 @@ def enter_website():
 
 
 enter_website()
-select_menu = driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/div[2]/div/div[2]/div/div/div/div/div[2]/div/ul/li[1]/div[2]/div/button[2]/span")
-drop = Select(select_menu)
-drop.select_by_index(2)
+
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/div[2]/div[1]/div[2]/div/div/select/option[3]").click()
 
 time.sleep(3)
-price = driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/div[2]/div[3]/div/div[1]/a[1]/div/div[5]/span[1]").get_attribute("mu-product-tile__price")
-name = driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/div[2]/div[3]/div/div[1]/a[1]/div/div[7]").get_attribute("mu-product-tile__name")
+price = driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/div[2]/div[3]/div/div[1]/a[1]/div/div[5]/span[1]").get_attribute("innerHTML")
+name = driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/div/div/div[2]/div[3]/div/div[1]/a[1]/div/div[7]").get_attribute("innerHTML")
 
 driver.quit()
 print(price, name)
